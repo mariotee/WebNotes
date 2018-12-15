463abc5ef10a576786fdbaa7392383c8f27d07a4
Mario Torres <mariotee10@gmail.com>
2018-12-15 12:35:49 -0600

    Updates

---
+++ precache-manifest.44c7f698c12f0b792536242cbe3e688a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1717ce794a165f7b0c316839c636ca4",
+    "url": "/webnotes/static/media/eyeslash.e1717ce7.svg"
+  },
+  {
+    "revision": "35347e34a6ee4b7ca0540ae8874556f2",
+    "url": "/webnotes/static/media/eye.35347e34.svg"
+  },
+  {
+    "revision": "4b6ad5963d73fd19ac3d",
+    "url": "/webnotes/static/js/runtime~main.4b6ad596.js"
+  },
+  {
+    "revision": "b9f049ad66a7310f75b1",
+    "url": "/webnotes/static/js/main.b9f049ad.chunk.js"
+  },
+  {
+    "revision": "dcbbcf74f44e896b720a",
+    "url": "/webnotes/static/js/1.dcbbcf74.chunk.js"
+  },
+  {
+    "revision": "b9f049ad66a7310f75b1",
+    "url": "/webnotes/static/css/main.5e652651.chunk.css"
+  },
+  {
+    "revision": "dcda6d54047b06baaa5ee8dafe43b7d5",
+    "url": "/webnotes/index.html"
+  }
+];
