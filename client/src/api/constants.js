fc49df1d5084e80fc6802f1d9095e4d0bb128477
Mario Torres <mariotee10@gmail.com>
2019-03-06 10:03:52 -0600

    client styling

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -1,2 +1,4 @@
-//export const LAMBDA_URL = "https://ju48kiggo7.execute-api.us-east-2.amazonaws.com/api"
-export const LAMBDA_URL = "https://1ztk64k2b4.execute-api.us-east-2.amazonaws.com/test"
+//main url
+export const LAMBDA_URL = "https://ju48kiggo7.execute-api.us-east-2.amazonaws.com/api"
+//test url
+//export const LAMBDA_URL = "https://1ztk64k2b4.execute-api.us-east-2.amazonaws.com/test"
